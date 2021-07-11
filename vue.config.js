18dcec57066e0cafc2c3609b000ceb64ad031bcf
Okiemute Gold <goldokiemute1@gmail.com>
2021-07-11 15:42:59 +0100

    new favicon from realfavicongenerator.net

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+module.exports = {
+    // ...other vue-cli plugin options...
+    pwa: {
+        themeColor: '#2b5797',
+        msTileColor: '#da532c',
+        iconPaths: {
+            msTileImage: 'img/icons/mstile-150x150.png'
+        }
+    }
+}
